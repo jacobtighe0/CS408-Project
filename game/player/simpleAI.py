27d048b9f55441b9790e7ce10aa0815ed6d99b96
Jacob <jacob.tighe.2021@uni.strath.ac.uk>
2025-01-14 16:35:56 +0000

    Non-AI players cards now randomized when simulating

--- game/player/simpleAI.py
+++ game/player/simpleAI.py
@@ -54,7 +54,7 @@ class SimpleAI(player.Player):
             chance = 0 # AI's chance of winning (initially 0)
             if self.game is not None:
 
-                loops = 1000 # Number of simulations
+                loops = 5000 # Number of simulations
                 mcts = MCTS(self)
                 i = 0
                 while i<loops:
@@ -132,6 +132,7 @@ class Node:
         self.value = 0
         self.wins = 0
         self.state = state
+        self.terminal = False
 
 class MCTS:
     
@@ -141,56 +142,56 @@ class MCTS:
         self.game = AI.game
 
     def run(self):
+        self.randomizeCards() # Randomizes cards for non-AI player
         selected_node = self.selection(self.root, self.root)
         self.expand(selected_node)
     
     def selection(self, node, highest_value_node):
+        if node.terminal == False:
+            if node.children != []:
+                for child in node.children:
+                    temp = self.selection(child, highest_value_node)
+                    if temp.value > highest_value_node.value:
+                        highest_value_node = temp
 
-        if node.children != []:
-            for child in node.children:
-                temp = self.selection(child, highest_value_node)
-                if temp.value > highest_value_node.value:
-                    highest_value_node = temp
+            if node.visits == 0:
+                node.value = float('inf')
+            else:
+                C = math.sqrt(2)
+                parent_visits = node.parent.visits if node.parent else 1
+                node.value = node.wins / node.visits + C * math.sqrt(math.log(parent_visits) / node.visits)
 
-        if node.visits == 0:
-            node.value = float('inf')
-        else:
-            C = math.sqrt(2)
-            parent_visits = node.parent.visits if node.parent else 1
-            node.value = node.wins / node.visits + C * math.sqrt(math.log(parent_visits) / node.visits)
-
-        if node.value > highest_value_node.value:
-            highest_value_node = node
+            if node.value > highest_value_node.value:
+                highest_value_node = node
         return highest_value_node
 
-            
     def expand(self, node):
-        new_node = Node(copy.deepcopy(node.state))
-        new_node.parent = node
+        if node.terminal == False:
+            new_node = Node(copy.deepcopy(node.state))
+            new_node.parent = node
 
-        new_node.state.dealer.cardControl.shuffle()
+            new_node.state.dealer.cardControl.shuffle()
 
-        if len(new_node.state.dealer.cardControl.tableCards) < 5:
-            if len(new_node.state.dealer.cardControl.tableCards) == 0:
-                new_node.state.dealer.cardControl.drawTable()
-                new_node.state.dealer.cardControl.drawTable()
-                new_node.state.dealer.cardControl.drawTable()
-            else:
-                new_node.state.dealer.cardControl.drawTable()
-        
-        node.children.append(new_node)
+            if len(new_node.state.dealer.cardControl.tableCards) < 5:
+                if len(new_node.state.dealer.cardControl.tableCards) == 0:
+                    new_node.state.dealer.cardControl.drawTable()
+                    new_node.state.dealer.cardControl.drawTable()
+                    new_node.state.dealer.cardControl.drawTable()
+                else:
+                    new_node.state.dealer.cardControl.drawTable()
 
-        self.simulate(new_node)
+            node.children.append(new_node)
+
+            self.simulate(new_node)
 
     def simulate(self, node):
-        simGame = copy.deepcopy(node.state)
+        node.state.dealer.cardControl.shuffle()
+        while len(node.state.dealer.cardControl.tableCards) < 5:
+            node.state.dealer.cardControl.drawTable()
 
-        simGame.dealer.cardControl.shuffle()
-        while len(simGame.dealer.cardControl.tableCards) < 5:
-            simGame.dealer.cardControl.drawTable()
-
-        winners = simGame.dealer.chooseWinner(simGame.players)
+        winners = node.state.dealer.chooseWinner(node.state.players)
         x = [winner.name for winner in winners]
+        node.terminal = True
         if x[0] == self.AIName:
             self.backpropagate(node, 1)
         else:
@@ -201,5 +202,16 @@ class MCTS:
         node.wins += result
         if node.parent is not None:
             self.backpropagate(node.parent, result)
+        
+    def randomizeCards(self):
+        for player in self.root.state.dealer.playerControl.players:
+            if player.name != self.AIName:
+                for c in player.hand:
+                    self.root.state.dealer.cardControl.deck.append(c) # Adds player's cards back to deck
+                player.hand = []
+                self.root.state.players = self.root.state.dealer.playerControl.players # Keeps it up-to-date
+                self.root.state.dealer.cardControl.shuffle()
+                self.root.state.dealer.cardControl.dealCard([player])
+                self.root.state.dealer.cardControl.dealCard([player])
 
         
