dc922cb37d414d9b5d61c2157539c152c6b0d18f
Jacob <jacob.tighe.2021@uni.strath.ac.uk>
2025-03-19 15:24:24 +0000

    removed old code

--- game/player/simpleAI.py
+++ game/player/simpleAI.py
@@ -149,32 +149,6 @@ class SimpleAI(player.Player):
                     action = 1
                 else:
                     action = 4
-
-            '''
-            if self.bet == -1:
-                if chance < .20:
-                    action = 4
-                else:
-                    action = 5
-            else:
-                if chance < .10:
-                    action = 4
-                elif chance < .50:
-                    if (self.checkBet() == False):
-                        action = 2
-                    else:
-                        action = 1
-                elif chance < .90:
-                    if self.prevAction == 3: # Stops AI from raising infinitely
-                        if (self.checkBet() == False):
-                            action = 2
-                        else:
-                            action = 1
-                    else:
-                        action = 3
-                else:
-                    action = 5
-            '''
                 
             choosed = options[action]()
             self.prevAction = action
@@ -183,32 +157,6 @@ class SimpleAI(player.Player):
             else:
                 continue
     
-    '''
-    def simulateGame(self):
-        import copy
-        simGame = copy.deepcopy(self.game)
-
-        player = simGame.dealer.playerControl.players[0] # Non-AI player
-        for c in player.hand:
-            simGame.dealer.cardControl.deck.append(c) # Adds player's cards back to deck
-        player.hand = []
-
-        simGame.players = simGame.dealer.playerControl.players # Makes sure simGame.players is up-to-date
-
-        simGame.dealer.cardControl.shuffle()
-        simGame.dealer.cardControl.dealCard([player])
-        simGame.dealer.cardControl.dealCard([player])
-
-        while len(simGame.dealer.cardControl.tableCards) < 5:
-            simGame.dealer.cardControl.drawTable()
-
-        winners = simGame.dealer.chooseWinner(simGame.players)
-        x = [winner.name for winner in winners]
-        if x[0] == self.name:
-            return 1
-        else:
-            return 0
-    '''
     
 class Node:
 
