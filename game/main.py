bf0d6e5c4f5dd38a97bce2d3a9e8cb11fad26cd5
Jacob <jacob.tighe.2021@uni.strath.ac.uk>
2025-03-21 17:47:24 +0000

    added 'play again' option

--- game/main.py
+++ game/main.py
@@ -8,45 +8,55 @@ def main():
     Work to create game
     """
     system('cls')
-    game = poker.Game()
     initialise_db()
-    print("")
-    print("Let's play Texas Hold'em!\n")
-    allPlayers = game.createPlayers()
     while True:
-        #The simple game functioning:
-        #Players bets on preflop (before the release of firt three cards)
-        #First three cards are released
-        #Another beting
-        #Turn-fourth card release
-        #Another bets
-        #River-final card
-        #Last beting
-        #Showdown-cards are showed (if any players are left)
-        print("\n\n\tRound #", game.rounds, end="\n\n") 
-        game.players = list(allPlayers)
-        player = game.players[0]
-        game.dealer.gameOn()
-        game.dealer.giveCards()
-        game.eachRound()
-        #Showdown
-        print("\n\t\tShowdown\n")
-        game.showdown()
-        game.dealer.endGame()
-        if len(game.dealer.playerControl.players) == 1:
-            print("")
-            print("Final winner is:", game.dealer.playerControl.players[0].name)
-            print("Money: ", int(game.dealer.playerControl.players[0].money))
-            print("")
-            win = False
-            if game.dealer.playerControl.players[0].name == player.name:
-                win = True
-            update_player_wins(player.name, win)
-            break
-        input("Press Enter to continue.")        
-        
-        game.rounds += 1
-        
+        game = poker.Game()
+        print("")
+        print("Let's play Texas Hold'em!\n")
+        allPlayers = game.createPlayers()
+        while True:
+            #The simple game functioning:
+            #Players bets on preflop (before the release of firt three cards)
+            #First three cards are released
+            #Another beting
+            #Turn-fourth card release
+            #Another bets
+            #River-final card
+            #Last beting
+            #Showdown-cards are showed (if any players are left)
+            print("\n\n\tRound #", game.rounds, end="\n\n") 
+            game.players = list(allPlayers)
+            player = game.players[0]
+            game.dealer.gameOn()
+            game.dealer.giveCards()
+            game.eachRound()
+            #Showdown
+            print("\n\t\tShowdown\n")
+            game.showdown()
+            game.dealer.endGame()
+            if len(game.dealer.playerControl.players) == 1:
+                print("")
+                print("Final winner is:", game.dealer.playerControl.players[0].name)
+                print("Money: ", int(game.dealer.playerControl.players[0].money))
+                print("")
+                win = False
+                if game.dealer.playerControl.players[0].name == player.name:
+                    win = True
+                update_player_wins(player.name, win)
+                break
+            input("Press Enter to continue.")        
+            
+            game.rounds += 1
+
+        while True:
+            play_again = input("Do you want to play again? (y/n): ").strip().lower()
+            if play_again == 'y':
+                break  # Continue the game if 'y' is entered
+            elif play_again == 'n':
+                print("\nGoodbye!\n")
+                return  # Exit the game if 'n' is entered
+            else:
+                print("Invalid input. Please enter 'y' for yes or 'n' for no.")
 
 if __name__ == "__main__":
     main()
